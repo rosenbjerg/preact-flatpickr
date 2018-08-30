e3524d94218f42215594776455700f0445e0610a
Malte Rosenbjerg <rosenbjerg@users.noreply.github.com>
2018-08-30 17:55:40 +0200

    Update preact-flatpickr.js

--- preact-flatpickr.js
+++ preact-flatpickr.js
@@ -1,105 +1,125 @@
 import { Component, createElement } from 'preact';
-import flatpickr from 'flatpickr';
+import PropTypes from 'prop-types'
+import Flatpickr from 'flatpickr'
 
-export default class Flatpickr extends Component {
-  constructor(props) {
-    super(props);
+const hooks = [
+    'onChange',
+    'onOpen',
+    'onClose',
+    'onMonthChange',
+    'onYearChange',
+    'onReady',
+    'onValueUpdate',
+    'onDayCreate'
+]
 
-    this.defaultOptions = {
-      theme: 'airbnb',
-    };
-
-    this.themes = {
-      dark: true,
-      material_blue: true,
-      material_green: true,
-      material_red: true,
-      material_orange: true,
-      airbnb: true,
-      confetti: true,
-    };
-
-    if (props.theme && !this.themes[props.theme]) {
-      console.error('Unsupported theme. Valid themes are: ' + Object.getOwnPropertyNames(this.themes).join(', '));
-    }
-    const theme = (props.theme && this.themes[props.theme]) ? props.theme : this.defaultOptions.theme;
-    import (`flatpickr/dist/themes/${theme}.css`);
-
-    this.state = {
-      config: props.config && {...props.config} || {},
-      instance: null,
-    };
-  }
-
-  render(props) {
-    return <input id="flatpickr-instance" />;
-  }
-
-  create() {
-    const instance = flatpickr('#flatpickr-instance', this.state.config);
-    const modifiedConfig = instance.config;
-
-    this.registerEvents(
-      [
-        'onChange',
-        'onClose',
-        'onDayCreate',
-        'onDestroy',
-        'onKeyDown',
-        'onMonthChange',
-        'onOpen',
-        'onParseConfig',
-        'onReady',
-        'onValueUpdate',
-        'onYearChange',
-        'onPreCalendarPosition',
-      ],
-      this.props,
-    );
-
-    this.setState({
-      config: { ...modifiedConfig, ...this.state.config },
-      instance,
-    });
-  }
-
-  recreate(props) {
-    this.state.instance.destroy();
-    this.create();
-  }
-
-  updateConfiguration(props) {
-    let config = this.state.instance.config;
-
-    if (props.config) {
-      config = { ...this.state.instance.config, ...props.config };
-      Object.keys(props.config).forEach(key => {
-        this.state.instance.set(key, props.config[key]);
-      });
+class DateTimePicker extends Component {
+    static propTypes = {
+        defaultValue: PropTypes.string,
+        options: PropTypes.object,
+        onChange: PropTypes.func,
+        onOpen: PropTypes.func,
+        onClose: PropTypes.func,
+        onMonthChange: PropTypes.func,
+        onYearChange: PropTypes.func,
+        onReady: PropTypes.func,
+        onValueUpdate: PropTypes.func,
+        onDayCreate: PropTypes.func,
+        value: PropTypes.oneOfType([
+            PropTypes.string,
+            PropTypes.array,
+            PropTypes.object,
+            PropTypes.number
+        ]),
+        children: PropTypes.node
     }
 
-    this.setState({
-      config
-    });
-  }
+    static defaultProps = {
+        options: {}
+    }
 
-  componentWillReceiveProps(props, state) {
-    this.updateConfiguration(props);
-    this.recreate(props);
-  }
+    componentWillReceiveProps(props) {
+        const { options } = props
+        const prevOptions = this.props.options
 
-  componentDidMount(state) {
-    this.create();
+        // Add prop hooks to options
+        hooks.forEach(hook => {
+            if (props.hasOwnProperty(hook)) {
+                options[hook] = props[hook]
+            }
+            // Add prev ones too so we can compare against them later
+            if (this.props.hasOwnProperty(hook)) {
+                prevOptions[hook] = this.props[hook]
+            }
+        })
 
-    // Recreating so the configuration gets set.
-    this.recreate(this.props);
-  }
+        const optionsKeys = Object.getOwnPropertyNames(options)
 
-  registerEvents(events, props) {
-    events.forEach(event => {
-      this.setState({
-        config: { ...this.state.config, [event]: props[event] },
-      });
-    });
-  }
+        for (let index = optionsKeys.length - 1; index >= 0; index--) {
+            const key = optionsKeys[index]
+            let value = options[key]
+
+            if (value !== prevOptions[key]) {
+                // Hook handlers must be set as an array
+                if (hooks.indexOf(key) !== -1 && !Array.isArray(value)) {
+                    value = [value]
+                }
+
+                this.flatpickr.set(key, value)
+            }
+        }
+
+        if (props.hasOwnProperty('value') && props.value !== this.props.value) {
+            this.flatpickr.setDate(props.value, false)
+        }
+    }
+
+    componentDidMount() {
+        const options = {
+            onClose: () => {
+                this.node.blur && this.node.blur()
+            },
+            ...this.props.options
+        }
+
+        // Add prop hooks to options
+        hooks.forEach(hook => {
+            if (this.props[hook]) {
+                options[hook] = this.props[hook]
+            }
+        })
+
+        this.flatpickr = new Flatpickr(this.node, options)
+
+        if (this.props.hasOwnProperty('value')) {
+            this.flatpickr.setDate(this.props.value, false)
+        }
+    }
+
+    componentWillUnmount() {
+        this.flatpickr.destroy()
+    }
+
+    render() {
+        // eslint-disable-next-line no-unused-vars
+        const { options, defaultValue, value, children, ...props } = this.props
+
+        // Don't pass hooks to dom node
+        hooks.forEach(hook => {
+            delete props[hook]
+        })
+
+        return options.wrap
+            ? (
+                <div {...props} ref={node => { this.node = node }}>
+                    { children }
+                </div>
+            )
+            : (
+                <input {...props} defaultValue={defaultValue}
+                       ref={node => { this.node = node }} />
+            )
+    }
 }
+
+export default DateTimePicker
